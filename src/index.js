cbf3d67365c1a8db23e9a7c3b2361a784bf039da
Hítalo <hitalo.taverna.quintal@gmail.com>
2022-06-15 22:05:19 -0300

    Exemplo de Filtro

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Filtro from './components/Filtro';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Es6/>
+    {/* <Es6/> */}
+    <Filtro/>
   </React.StrictMode>
 );
 
